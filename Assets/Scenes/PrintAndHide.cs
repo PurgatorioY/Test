90953ba58be2f7ae164e7ce8ee41cdfa40aa5bab
Yi Lin <yi.lin-1@student.uts.edu.au>
2021-08-29 23:17:20 +1000

    100 percent complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -6,10 +6,11 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int random;
     // Start is called before the first frame update
     void Start()
     {
-     
+        random = Random.Range(200, 251); 
     }
 
     // Update is called once per frame
@@ -17,6 +18,18 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
-        
+
+//        GameObject.FindWithTag();
+
+//        Debug.Log(GameObject.FindGameObjectsWithTag("Red"));
+        if (i == 100 && gameObject.CompareTag("Red"))
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (i == random && gameObject.CompareTag("Blue"))
+        {
+            rend.enabled = false;
+        }
     }
 }
