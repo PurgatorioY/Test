08658e7831926b96f9eb0e2ca0e0b4c29e449f90
Yi Lin <yi.lin-1@student.uts.edu.au>
2021-08-29 21:56:08 +1000

    80 percent complete

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -11,13 +11,13 @@ public class LoadAssets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
-        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
     }
 }
